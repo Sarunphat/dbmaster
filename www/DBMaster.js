c05a3b32df7012996f7237f2867037fc73cc7447
Sarunphat <36180449+Sarunphat@users.noreply.github.com>
2018-11-06 17:03:05 +0700

    Update DBMaster.js

--- www/DBMaster.js
+++ www/DBMaster.js
@@ -1,5 +1,6 @@
 var dbMaster = {
     loadDatabase: (successCallback, errorCallback) => {
+        console.log("dbMaster starts loading database.");
         cordova.exec(
             successCallback,
             errorCallback,
