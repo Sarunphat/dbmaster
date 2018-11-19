4af00aae1e1a01b730d2ab5cef724dd6604e5df4
Sarunphat <36180449+Sarunphat@users.noreply.github.com>
2018-11-19 10:37:18 +0700

    Update DBMasterPlugin.kt

--- src/DBMasterPlugin.kt
+++ src/DBMasterPlugin.kt
@@ -4,6 +4,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.util.Log
 
 import mobileapp.com.dbm.DBMaster;
 
